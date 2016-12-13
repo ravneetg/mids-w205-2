2eb2077855136c5ccbc18acb57bf7f830847432e
ravneetg <ravneetghuman@ischool.berkeley.edu>
2016-12-12 22:12:53 -0700

    delete

--- twitter/config_template.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "XxXxXxxXXXxxxxXXXxXX"
-consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
-access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
-access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
